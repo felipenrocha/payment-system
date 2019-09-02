b21bfe8e6c413776c849f8a06eb43fc9c734bb91
felipenrocha <felipenrocha19@gmail.com>
2019-09-02 14:22:39 -0300

    Classe cartao de crédito

---
+++ cartao.hpp
@@ -0,0 +1,29 @@
+#ifndef __CARTAO_HPP__
+#define __CARTAO_HPP__
+#include <iostream>
+#include <string>
+#include <ctype.h>
+#include <list>
+using namespace std;
+
+class Cartao
+{
+private:
+    string numero;
+    string codigo;
+    string dataValidade;
+
+protected:
+    int checaAlgoritmoLuhn(string number);
+
+public:
+    Cartao(string number, string code, string date);
+    int numeroValido(string number);
+    int codigoValido(string code);
+    int dataValida(string date);
+    string getNumero() { return this->numero; }
+    string getCodigo() { return this->codigo; }
+    string getData() { return this->dataValidade; }
+};
+
+#endif
