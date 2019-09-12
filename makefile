d7be9b3b50f39b43a54342eb55c268dc39665c61
felipenrocha <felipenrocha19@gmail.com>
2019-09-12 10:22:07 -0300

    finalizacao teste_ingresso

--- makefile
+++ makefile
@@ -1,14 +1,23 @@
 CC = g++
 LIBS = -Wall -g -lgtest_main -lgtest -lpthread -pthread
+Usuario = teste_usuario teste_usuario.cpp
+Partida = teste_partida teste_partida.cpp
+Cartao =  teste_cartao teste_cartao.cpp
+Ingresso = teste_ingresso teste_ingresso.cpp
+
 
 testeUsuario:
-	${CC} -o teste_usuario teste_usuario.cpp ${LIBS}
+	${CC} -o  $(Usuario) ${LIBS}
 	./teste_usuario
 
 testePartida:
-	${CC} -o teste_partida teste_partida.cpp ${LIBS}
+	${CC} -o $(Partida) ${LIBS}
 	./teste_partida
 
 testeCartao:
-	${CC} -o teste_cartao teste_cartao.cpp ${LIBS}
+	${CC} -o $(Cartao) ${LIBS}
 	./teste_cartao
+
+testeIngresso:
+	$(CC) -o $(Ingresso) $(LIBS)
+	./teste_ingresso
---
+++ teste_ingresso.cpp
@@ -0,0 +1,69 @@
+#include "ingresso.cpp"
+#include <gtest/gtest.h>
+
+// compile: g++ -Wall -g -pthread -o teste_ingresso teste_ingresso.cpp -lgtest_main -lgtest -lpthread
+
+int main(int argc, char **argv)
+{
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(CadastroIngresso, CodigoValido)
+{
+    Ingresso ingresso("01234");
+    ASSERT_EQ("01234", ingresso.getCodigo());
+    ASSERT_EQ(true, ingresso.codigoValido(ingresso.getCodigo()));
+
+    Ingresso ingresso2("11229");
+    ASSERT_EQ("11229", ingresso2.getCodigo());
+    ASSERT_EQ(true, ingresso2.codigoValido(ingresso2.getCodigo()));
+}
+
+TEST(Exception, CodigoInvalidoLetras)
+
+{
+    EXPECT_THROW({
+        try
+        {
+            Ingresso ingresso("A1234"); // Letras não sao permitidas
+        }
+        catch (invalid_argument e)
+        {
+            EXPECT_STREQ("Certifique-se de que seja um código válido (Formato XXXXX onde X é dígito (0 – 9)).", e.what());
+            throw;
+        }
+    },
+                 invalid_argument);
+}
+
+TEST(Exception, CodigoInvalidoTamanho)
+{
+
+    EXPECT_THROW({
+        try
+        {
+            Ingresso ingresso("111234"); // Tamanho do codigo != 5
+        }
+        catch (invalid_argument e)
+        {
+            EXPECT_STREQ("Certifique-se de que seja um código válido (Formato XXXXX onde X é dígito (0 – 9)).", e.what());
+            throw;
+        }
+    },
+                 invalid_argument);
+
+    EXPECT_THROW({
+        try
+        {
+            Ingresso ingresso("1234"); // Tamanho do codigo != 5
+        }
+        catch (invalid_argument e)
+        {
+            EXPECT_STREQ("Certifique-se de que seja um código válido (Formato XXXXX onde X é dígito (0 – 9)).", e.what());
+            throw;
+        }
+    },
+                 invalid_argument);
+}
