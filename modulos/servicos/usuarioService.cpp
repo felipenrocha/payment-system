d9b3d11d8590144ceb899c2481866e6de5163a7f
felipenrocha <felipenrocha19@gmail.com>
2019-10-22 22:59:08 -0300

    adaptacao-tui

--- modulos/servicos/usuarioService.cpp
+++ modulos/servicos/usuarioService.cpp
@@ -12,30 +12,6 @@ UsuarioInterface::~UsuarioInterface()
 
 void UsuarioInterface::autenticacao()
 {
-
-    int input;
-    do
-    {
-        cout << "Selecione uma opção:" << endl;
-        cout << "1) Entrar" << endl;
-        cout << "2) Cadastrar" << endl;
-        cout << "3) Fechar" << endl;
-        cin >> input;
-        if (input != 1 && input != 2 && input != 3)
-        {
-            cout << "Por favor, selecione uma opção válida!" << endl;
-        }
-
-    } while (input != 1 && input != 2 && input != 3);
-
-    if (input == 1)
-    {
-        loginUsuario();
-    }
-    else if (input == 2)
-    {
-        registrarUsuario();
-    }
 }
 void UsuarioInterface::create()
 {
